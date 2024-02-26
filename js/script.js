fda63df6af46b4fd4f36075da63e9cd7620050eb
Leonardo Vaudagna <leovaudagna92@gmail.com>
2024-02-26 14:48:55 -0300

    UI modifications

--- js/script.js
+++ js/script.js
@@ -1,66 +1,127 @@
+//GLOBAL VARIABLES
+
+let you = 0;
+let cpu = 0;
+
+
+const yourScore = document.getElementById('your-score');
+yourScore.innerHTML = you;
+const cpuScore = document.getElementById('cpu-score');
+cpuScore.innerHTML = cpu;
+
+
+//CPU RANDOM CHOICE
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     
     switch (randomNumber){
         case 0:
-            return "Piedra";
+            return "Rock";
             break;
         case 1:
-            return "Papel";
+            return "Paper";
             break;
         case 2:
-            return "Tijera";
+            return "Scissors";
             break;
     }
 }
+
+//SCORE
+function score(text){
+    if (text == "YOU WON"){
+        you += 1;
+    } else if(text == "YOU LOSE"){
+        cpu += 1;
+    } else {
+        console.log("DRAW");
+    }
+
+    yourScore.innerText = you;
+    cpuScore.innerText = cpu;
+
+    if(you == 5){
+        console.log("PEPE");
+        document.getElementById('final-score').innerText = "YOU WON! CONGRATULATIONS";
+    } else if(cpu == 5){
+        document.getElementById('final-score').innerText = "YOU LOSE!";
+    }
+    
+}
+
+
 
 function playerSelection(me, cpu){
     
     let cpuChoice = cpu.toLowerCase();
     let playerChoice = me.toLowerCase();
     
-    console.log("JUGADOR: " + playerChoice);
-    console.log("CPU: " + cpuChoice);
+
+    document.getElementById('yourChoice').innerText = playerChoice;
+    document.getElementById('cpuChoice').innerText = cpuChoice;
+
+    let result = document.getElementById('result');
     
 
     switch (playerChoice){
-        case "piedra":
-            if(cpuChoice == "papel"){
-                return "PERDISTE";
+        case "rock":
+        if(cpuChoice == "paper"){
+                result.innerText = "YOU LOSE";
                 break;
-            } else if (cpuChoice == "tijera"){
-                return "GANASTE";
+            } else if (cpuChoice == "scissors"){
+                result.innerText = "YOU WON";
                 break;
             } else {
-                return "EMPATE";
+                result.innerText = "DRAW";
                 break;
             }
         
-        case "papel":
-            if(cpuChoice == "tijera"){
-                return "PERDISTE";
+        case "paper":
+            if(cpuChoice == "scissors"){
+                result.innerText = "YOU LOSE";
                 break;
-            } else if (cpuChoice == "piedra"){
-                return "GANASTE";
+            } else if (cpuChoice == "rock"){
+                result.innerText = "YOU WON";
                 break;
             } else {
-                return "EMPATE";
+                result.innerText = "DRAW";
                 break;
             }
         
-        case "tijera":
-            if(cpuChoice == "piedra"){
-                return "PERDISTE";
+        case "scissors":
+            if(cpuChoice == "rock"){
+                result.innerText = "YOU LOSE";
                 break;
-            } else if (cpuChoice == "papel"){
-                return "GANASTE";
+            } else if (cpuChoice == "paper"){
+                result.innerText = "YOU WON";
                 break;
             } else {
-                return "EMPATE";
+                result.innerText = "DRAW";
                 break;
             }
     }
+
+    console.log(result.innerText);
+    score(result.innerText);
+    return result;
 }
 
-let resultado = playerSelection("Piedra", getComputerChoice());
-console.log(resultado);
+
+//Nueva lógica
+const rock = document.getElementById("rock-button");
+const paper = document.getElementById("paper-button");
+const scissors = document.getElementById("scissors-button");
+
+//EVENT LISTENERS
+rock.addEventListener("click", function(){
+    playerSelection("rock", getComputerChoice());
+})
+
+paper.addEventListener("click", function(){
+    playerSelection("paper", getComputerChoice());
+})
+
+scissors.addEventListener("click", function(){
+    playerSelection("scissors", getComputerChoice());
+})
+
